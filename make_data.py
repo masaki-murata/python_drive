0e5aaa2acfc8b381ad43ec180eaed68f9dedf1e2
Masaki Murata <masaki-murata@umin.ac.jp>
2018-02-22 16:40:39 +0900

    no message

--- make_data.py
+++ make_data.py
@@ -5,5 +5,13 @@ Created on Thu Feb 22 16:23:25 2018
 @author: murata
 """
 
+import numpy as np
+from PIL import Image, ImageOps
+
 def load_data():
-    path_to_train_images = "../training/
+    path_to_train_images = "../training/images/%d_training.tif"
+    for image_id in range(21, 41):
+        image = np.array( Image.open(path_to_train_images % image_id) )
+        print(image.shape)
+        
+load_data()
