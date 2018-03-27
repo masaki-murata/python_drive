d61045da3a8d5e9e95f5e75af6e99397d92b7479
MasakiMurata <muratamasaki@MasakiMurata.local>
2018-03-27 23:45:41 +0900

    no message

--- seunet_main.py
+++ seunet_main.py
@@ -11,7 +11,7 @@ def dice_coef(y_true, y_pred):
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
     intersection = K.sum(y_true_f * y_pred_f)
-    return (2. * intersection +1) / (K.sum(y_true_f) + K.sum(y_pred_f) + 1)
+    return (2. * intersection +1) / (K.sum(y_true_f) + K.sum(y_pred_f) + 1.)
 
 def dice_coef2(y_true, y_pred):
     y_true_f = K.flatten(y_true)
