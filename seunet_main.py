3ec9c685632fc7c135c5bd73c5d90fc668343adb
Masaki Murata <masaki-murata@umin.ac.jp>
2018-03-27 18:02:44 +0900

    no message

--- seunet_main.py
+++ seunet_main.py
@@ -13,6 +13,12 @@ def dice_coef(y_true, y_pred):
     intersection = K.sum(y_true_f * y_pred_f)
     return (2. * intersection +1) / (K.sum(y_true_f) + K.sum(y_pred_f) + 1)
 
+def dice_coef2(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return 0.5*dice_coef(y_true, y_pred) + 0.5*dice_coef(1-y_true, 1-y_pred)
+
 
 def mean_dice_coef_loss(y_true, y_pred):
 
@@ -21,7 +27,7 @@ def mean_dice_coef_loss(y_true, y_pred):
     loss = 0
 
     for ch in range(channels_nb):
-        loss = loss + dice_coef(y_true[:,:,:,ch], y_pred[:,:,:,ch])
+        loss = loss + dice_coef2(y_true[:,:,:,ch], y_pred[:,:,:,ch])
 
     return 1 - loss/channels_nb
 
